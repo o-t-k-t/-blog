d8804688aff692d6de30c7aae7a872d3a31e4d2f
Toshiki Okabe <t.okabe1986@gmail.com>
2018-09-27 07:20:03 +0000

    Contentモデルのバリデーションの冗長な記述を修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
-  validates :content, presence: true
-  validates :content, length: {
+  validates :content, presence: true, length: {
     minimum: 1, too_short: '1文字以上入力してください',
     maximum: 140, too_long: '最大140文字まで入力できます'
   }
